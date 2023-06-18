cd6fe947b5b9c5b2a7e9c042aa33871e9413fd5e
huraicid <96579570+huraicid@users.noreply.github.com>
2023-06-18 20:59:35 +0900

    名刺用のソースコード

---
+++ Profile.java
@@ -0,0 +1,23 @@
+package fff;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Profile {
+    // 名前
+    private String name = "風来のシド";
+    
+    // 担当
+    private List<String> produceOf = Arrays.asList(
+        "佐久間まゆ", "三船美優", "藤原肇", "依田芳乃"
+    );
+    
+    // 所在地
+    private String branch = "Hiroshima";
+    
+    // リンク
+    private List<String> link = Arrays.asList(
+        "@huraicid",
+        "https://github.com/huraicid"
+    );
+}
